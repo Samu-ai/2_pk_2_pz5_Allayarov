088c79a5ed64c62993fb1474f26057b7a71401fa
Samu-ai <114611303+Samu-ai@users.noreply.github.com>
2022-09-28 23:05:09 +0500

    Add files via upload

---
+++ 2_пк_2_пз5_Аллаяров/2_пк_2_пз5_Аллаяров/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _2_пк_2_пз5_Аллаяров
+{
+    internal class Program
+    {
+        static void Main(string[] args);
+        //{
+        //    int a = 0;
+        //    int b = 80;
+        //    for (; a <= b; a+=4)
+        //    {
+        //        Console.WriteLine(a.ToString());
+        //    }
+        //        Console.ReadKey();
+        //}
+
+        //char symbol = 'b';
+        //string result = "";
+        //for (int i = 0; i< 6; i++)
+        //{
+        //    result += Convert.ToChar(symbol + i);
+        //}
+        //Console.WriteLine(result);
+
+        //for (int i = 0; i < 6; i++)
+        //    {
+        //        for (int j = 0; j< 5; j++)
+        //        {
+        //            Console.Write("#");
+        //        }
+        //Console.WriteLine();
+        //    }
+        //Console.ReadKey();
+
+        //int a = 0;
+        //int b = 100;
+        //    for (; a <= b; a += 5)
+        //    {
+        //        Console.Write(a.ToString() + " ");
+        //    }
+        //Console.ReadKey();
+
+        // for (int i = 1, j = 40; Math.Abs(i - j) != 15; i++, j--)
+        //    {
+        //        Console.WriteLine("{0} {1}", i, j);
+        //    }
+}
+}
